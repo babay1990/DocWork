3a7d7476635b944e07bbaf77f79d0869593ac330
Alexandr Shpagin <babay1990@yandex.ru>
2021-01-26 16:13:01 +0300

    new

--- src/main/java/com/shpaginWork/docWork/storage/StorageProperties.java
+++ src/main/java/com/shpaginWork/docWork/storage/StorageProperties.java
@@ -8,7 +8,7 @@ public class StorageProperties {
     /**
      * Folder location for storing files
      */
-    private String location = "D:/Spring/docWork/files";
+    private String location = "/files";
 
     public String getLocation() {
         return location;
