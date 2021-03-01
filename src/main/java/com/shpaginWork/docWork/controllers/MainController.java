be4a5bafa700750465b67a3b9c94cc3e11718a4a
Alexandr Shpagin <babay1990@yandex.ru>
2021-03-01 16:52:28 +0300

    Jsoup

--- src/main/java/com/shpaginWork/docWork/controllers/MainController.java
+++ src/main/java/com/shpaginWork/docWork/controllers/MainController.java
@@ -1,14 +1,21 @@
 package com.shpaginWork.docWork.controllers;
 
+import com.shpaginWork.docWork.CustomUserDetailService;
 import com.shpaginWork.docWork.models.News;
+import com.shpaginWork.docWork.models.Users;
 import com.shpaginWork.docWork.repo.NewsRepository;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 //контроллер главной страницы
@@ -18,13 +25,39 @@ public class MainController {
     @Autowired
     private NewsRepository newsRepository;
 
+    @Autowired
+    private CustomUserDetailService userService;
+
     //главная страница
     @GetMapping("/")
-    public String main(Model model) {
+    public String main(Model model) throws IOException {
 
         //Передаем объект Iterable, содержащий все новости, на страницу
         Iterable<News> block = newsRepository.findAll();
         model.addAttribute("block", block);
+
+
+        List<String> list = new ArrayList<String>();
+
+        Document doc = Jsoup.connect("https://yandex.ru/").get();
+
+        Elements el = doc.getElementsByAttributeValue("class", "inline-stocks__value");
+
+        el.forEach(ell ->{
+
+            String usd = ell.child(1).text();
+            list.add(usd);
+        });
+
+        String usd = list.get(0);
+        String eur = list.get(1);
+
+        model.addAttribute("usd", usd);
+        model.addAttribute("eur", eur);
+
+        list.clear();
+
+
 
         return "main";
     }
